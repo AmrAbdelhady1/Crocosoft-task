76721b411ab966edf1a32d6f2b69410b8c2b0fed
AmrAbdelhady1 <amr@blazi.co>
2024-09-03 12:12:37 +0300

    .

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = `https://json-server-one-beta.vercel.app/`;
+export const BASE_URL = `https://json-server-one-beta.vercel.app`;
 
 export const validYoutubeUrlRegex =
   /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\\-]+\?v=|embed\/|live\/|v\/)?)([\w\\-]+)(\S+)?$/;
