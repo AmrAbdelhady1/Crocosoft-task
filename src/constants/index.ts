ab84fda253be7b7bde5fb7d6e71a580f0fb98d5a
AmrAbdelhady1 <amr@blazi.co>
2024-09-03 13:28:58 +0300

    .

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = `https://restful-api-vercel-glgcd4c8i-amrabdelhady1s-projects.vercel.app`;
+export const BASE_URL = `http://localhost:3001`;
 
 export const validYoutubeUrlRegex =
   /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\\-]+\?v=|embed\/|live\/|v\/)?)([\w\\-]+)(\S+)?$/;
