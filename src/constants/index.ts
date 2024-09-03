875d56e39b3c848ece35b3cd3a64b7f7539ccc48
AmrAbdelhady1 <amr@blazi.co>
2024-09-03 11:54:24 +0300

    fix deployment

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = `http://localhost:3001`;
+export const BASE_URL = `/api`;
 
 export const validYoutubeUrlRegex =
   /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\\-]+\?v=|embed\/|live\/|v\/)?)([\w\\-]+)(\S+)?$/;
