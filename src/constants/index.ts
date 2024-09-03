fdf98241217145371343aa240b47edf2bbb142ce
AmrAbdelhady1 <amr@blazi.co>
2024-09-03 12:07:56 +0300

    fix deployment

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = `/api`;
+export const BASE_URL = `https://json-server-one-beta.vercel.app/quizzes`;
 
 export const validYoutubeUrlRegex =
   /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\\-]+\?v=|embed\/|live\/|v\/)?)([\w\\-]+)(\S+)?$/;
